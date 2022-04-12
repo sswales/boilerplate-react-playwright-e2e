5f34e49fe85fab33d2427e8aa5caa205409194d2
Shaun Swales <saswales@icloud.com>
2022-04-12 14:42:20 +0100

    Added One-Time setup/teardown options & .it to .test to be more readable to newbies.

--- e2e/specs/home-page.spec.js
+++ e2e/specs/home-page.spec.js
@@ -1,11 +1,12 @@
-import { getIntroText, getLinkText } from "../pageObjects/app";
+import { getIntroText, getLinkText } from '../pageObjects/app';
 
-describe("App", () => {
-    it("should show the correct landing page", async () => {
-        expect(await getIntroText()).toBe('Use this boilerplate package to e2e test your React app.');
-        expect(await getLinkText()).toBe('E2E Boilerplate for React');
-    });
-
-    // it("should show the correct link", async () => {
-    // });
+describe('App', () => {
+  test('should show the page logo', async () => {
+    expect(await getIntroText()).toBe('Use this boilerplate package to e2e test your React app.');
+    expect(await getLinkText()).toBe('E2E Boilerplate for React');
+  });
+  test('should show the page introduction', async () => {
+    expect(await getIntroText()).toBe('Use this boilerplate package to e2e test your React app.');
+    expect(await getLinkText()).toBe('E2E Boilerplate for React');
+  });
 });
--- e2e/utils/setupTests.js
+++ e2e/utils/setupTests.js
@@ -1,9 +1,21 @@
 import { load, close } from '../pageObjects';
 
+// Before Each Test
 beforeEach(async () => {
   await load();
 });
 
+// After Each Test
 afterEach(async () => {
   await close();
+});
+
+// One-Time Setup
+beforeAll(() => {
+  // Start Env
+});
+
+// One-Time Teardown
+afterAll(() => {
+  // Stop Env
 });
