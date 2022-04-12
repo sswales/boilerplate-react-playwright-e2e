86fcb0bf14bcadcc553d12d6af13642731d84044
Shaun Swales <saswales@icloud.com>
2022-04-12 14:26:04 +0100

    Updated jest config with maxWorkers and unit test command.

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -2,10 +2,11 @@ module.exports = {
   globals: {
     baseURL: 'http://localhost:3000',
   },
-  testMatch: ['**/specs/*.js'],
+  testMatch: ['**/specs/*.spec.js'],
   transform: {
     '\\.js$': 'react-scripts/config/jest/babelTransform',
   },
   verbose: true,
   setupFilesAfterEnv: ['<rootDir>/utils/setupTests.js'],
+  maxWorkers: '50%',
 };
